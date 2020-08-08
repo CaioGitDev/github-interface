0ad20960c1142696c523b63dfb7ffe8c81485040
CaioDev <53819717+CaioGitDev@users.noreply.github.com>
2020-08-08 17:51:19 +0100

    feat: add profile component

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,10 +1,28 @@
 import React from 'react'
-import { Container } from './styles'
+import { Container, Main, LeftSide, RightSide } from './styles'
+import ProfileData from '../../components/ProfileData'
 
 const Profile: React.FC = () => {
   return(
     <Container>
+      <Main>
+        <LeftSide>
+          <ProfileData 
+            username={'CaioGitDev'}
+            name={'Caio Rosa'}
+            avatarUrl={'https://avatars2.githubusercontent.com/u/53819717?v=4'}
+            followers={666}
+            following={0}
+            company={'CRDevLab'}
+            location={'Lisboa, Portugal'}
+            email={'geral-caio@gmail.com'}
+            blog={'https://www.linkedin.com/in/caio-rosa-4047b1137/'}
+          />
+        </LeftSide>
+        <RightSide>
 
+        </RightSide>
+      </Main>
     </Container>
   )
 }
