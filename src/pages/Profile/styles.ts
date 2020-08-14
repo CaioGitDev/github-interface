a277b416839346d6c7c0a2a366a83035216e3406
CaioDev <53819717+CaioGitDev@users.noreply.github.com>
2020-08-14 21:32:12 +0100

    feat: add grid template columns based on min width

--- src/pages/Profile/styles.ts
+++ src/pages/Profile/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.div`
   --horizontalPadding: 16px;
@@ -14,20 +14,19 @@ export const Main = styled.div`
   margin: 0 auto;
   max-width: 1280px;
 
-  @media (min-width: 768px){
-   flex-direction: row;
- }
-
+  @media (min-width: 768px) {
+    flex-direction: row;
+  }
 `;
 export const LeftSide = styled.div`
- @media (min-width: 768px){
-   width: 25%;
- }
+  @media (min-width: 768px) {
+    width: 25%;
+  }
 `;
 export const RightSide = styled.div`
- @media (min-width: 768px){
-   width: 75%;
- }
+  @media (min-width: 768px) {
+    width: 75%;
+  }
 `;
 
 export const Repos = styled.div`
@@ -36,7 +35,6 @@ export const Repos = styled.div`
   > h2 {
     font-size: 16px;
     font-weight: normal;
-
   }
 
   > div {
@@ -44,5 +42,10 @@ export const Repos = styled.div`
     display: grid;
     grid-gap: 16px;
     grid-template-columns: 1fr;
+
+    @media (min-width: 768px) {
+      grid-template-columns: 1fr 1fr;
+      grid-auto-rows: minmax(min-content, max-content);
+    }
   }
 `;
