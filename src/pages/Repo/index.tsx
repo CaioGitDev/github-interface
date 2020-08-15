38a6fc3ffa5a586eea649213e099f7758f01a2b2
CaioDev <53819717+CaioGitDev@users.noreply.github.com>
2020-08-15 20:06:47 +0100

    feat: create and customize repo component

--- src/pages/Repo/index.tsx
+++ src/pages/Repo/index.tsx
@@ -1,12 +1,53 @@
-import React from 'react'
-import { Container } from './styles'
+import React from "react";
+import { Link } from 'react-router-dom';
+import {
+  Container,
+  Breadcrumb,
+  RepoIcon,
+  Stats,
+  StarIcon,
+  ForkIcon,
+  LinkButton,
+  GithubIcon,
+} from "./styles";
 
-const Repo: React.FC = () => {
+interface Props {
+  username?: string;
+  reponame?: string;
+}
+const Repo: React.FC<Props> = ({ username, reponame }) => {
   return (
     <Container>
+      <Breadcrumb>
+        <RepoIcon />
+        <Link className={"username"} to={`/${username}`}>
+          CaioGitDev
+        </Link>
+        <span>/</span>
+        <Link className={"reponame"} to={`/${username}/${reponame}`}>
+          {reponame}
+        </Link>
+      </Breadcrumb>
 
+      <p>Contains all my...</p>
+      <Stats>
+        <li>
+          <StarIcon />
+          <b>9</b>
+          <span>Stars</span>
+        </li>
+        <li>
+          <ForkIcon />
+          <b>96</b>
+          <span>Forks</span>
+        </li>
+      </Stats>
+      <LinkButton href={`https://github.com/${username}/${reponame}`}>
+        <GithubIcon />
+        <span>View on GitHub</span>
+      </LinkButton>
     </Container>
-  )
-}
+  );
+};
 
-export default Repo
+export default Repo;
