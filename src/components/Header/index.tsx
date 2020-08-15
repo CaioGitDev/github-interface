4222458815f392e10b1323249a387f9f0b251ab7
CaioDev <53819717+CaioGitDev@users.noreply.github.com>
2020-08-15 23:25:26 +0100

    feat: add toggle theme

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,26 +1,38 @@
-import React, { useState } from  'react'
-import { Container, GithubLogo, SearchForm } from './styles'
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { Container, GithubLogo, SearchForm } from "./styles";
+import { useNavigate } from "react-router-dom";
+import { ThemeName } from "../../styles/themes";
 
-const Header: React.FC = () => {
-  const [search, setSearch] = useState('');
+interface Props {
+  themeName: ThemeName;
+  setThemeName: (newName: ThemeName) => void;
+}
+
+const Header: React.FC<Props> = ({ themeName, setThemeName }) => {
+  const [search, setSearch] = useState("");
   const navigate = useNavigate();
 
-  function handleSubmit (event: React.FormEvent) {
+  function handleSubmit(event: React.FormEvent) {
     event.preventDefault();
-    navigate('/' + search.toLowerCase().trim());
+    navigate("/" + search.toLowerCase().trim());
   }
+
+  function toggleTheme() {
+    setThemeName(themeName === "light" ? "dark" : "light");
+  }
+
   return (
     <Container>
-      <GithubLogo />
+      <GithubLogo onClick={toggleTheme} />
       <SearchForm onSubmit={handleSubmit}>
-        <input placeholder="Enter Username or Repo..." 
-        value={search}
-        onChange={ e => setSearch(e.currentTarget.value)}
+        <input
+          placeholder="Enter Username or Repo..."
+          value={search}
+          onChange={(e) => setSearch(e.currentTarget.value)}
         />
       </SearchForm>
     </Container>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
